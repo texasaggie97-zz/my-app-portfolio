6b1a478712bc4a13fc61c666f566a9baec1e4510
Mark Silva <bitbucket@markesilva.com>
2015-06-08 22:49:03 -0500

    Initial commit

---
+++ app/src/main/java/com/markesilva/myappportfolio/MainActivity.java
@@ -0,0 +1,71 @@
+package com.markesilva.myappportfolio;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // on click implementations for all buttons
+    public void onClickMediaStreamer(View view)
+    {
+        Toast.makeText(getApplicationContext(), R.string.toast_spotify, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickSuperDuo1(View view)
+    {
+        Toast.makeText(getApplicationContext(), R.string.toast_scores, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickSuperDuo2(View view)
+    {
+        Toast.makeText(getApplicationContext(), R.string.toast_library, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickBigger(View view)
+    {
+        Toast.makeText(getApplicationContext(), R.string.toast_build, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickReader(View view)
+    {
+        Toast.makeText(getApplicationContext(), R.string.toast_reader, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickCapstone(View view)
+    {
+        Toast.makeText(getApplicationContext(), R.string.toast_capstone, Toast.LENGTH_SHORT).show();
+    }
+}
